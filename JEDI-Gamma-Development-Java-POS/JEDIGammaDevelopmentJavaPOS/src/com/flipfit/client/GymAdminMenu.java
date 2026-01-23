b541437f68b288ea31cef827fb148aab9c22d2de
akilesh1706 <skakilesh2004@gmail>
2026-01-23 14:33:39 +0530

    add client classes

---
+++ JEDI-Gamma-Development-Java-POS/JEDIGammaDevelopmentJavaPOS/src/com/flipfit/client/GymAdminMenu.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+package com.flipfit.client;
+
+/**
+ * 
+ */
+public class GymAdminMenu {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
